57f8c13adcb55f2930aeaeb362e0b94fe425a0c5
ambarwati <ambarwati2896@gmail.com>
2021-08-22 23:48:46 +0700

    add first_name column and last_name column

--- schema.sql
+++ schema.sql
@@ -1,5 +1,7 @@
 CREATE TABLE users (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
+    created_at TIMESTAMP DEFAULT NOW(),
+    first_name varchar(255),
+    last_name varchar(255)
 );
