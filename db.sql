81d14460e651b720ccf90228acb5de32feaee729
ambarwati <ambarwati2896@gmail.com>
2021-08-22 23:52:23 +0700

    insert some data in db.sql

--- db.sql
+++ db.sql
@@ -1,1 +1,4 @@
 insert into users (username, first_name, last_name) values (rahmat_digital_skola, rahmat, hidayat);
+insert into users (username, first_name, last_name) values (lela_digital_skola, lela, sari);
+insert into users (username, first_name, last_name) values (kurniawan_digital_skola, kurniawan, agung);
+insert into users (username, first_name, last_name) values (adam_digital_skola, adam, makmur);
