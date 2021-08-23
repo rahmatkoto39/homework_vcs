0727ba59f6e651c24fc3c88bf48f3cc36354ca87
dito-setyadhi <setyadhidito29@gmail.com>
2021-08-23 16:15:47 +0700

    Muhammad Dito Setyadhi - Homework2

--- schema.sql
+++ schema.sql
@@ -5,3 +5,9 @@ CREATE TABLE users (
     first_name varchar(255),
     last_name varchar(255)
 );
+
+alter tabel users
+add column first_name VARCHAR(100) not null,
+add column last_name VARCHAR(100) not null;
+
+
