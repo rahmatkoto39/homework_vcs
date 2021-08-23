9f3d2963db5d20de6c40ad5f96ffa15ce5659223
dito-setyadhi <setyadhidito29@gmail.com>
2021-08-23 16:28:04 +0700

    Muhammad Dito Setyadhi - HW02

--- db.sql
+++ db.sql
@@ -2,3 +2,5 @@ insert into users (username, first_name, last_name) values (rahmat_digital_skola
 insert into users (username, first_name, last_name) values (lela_digital_skola, lela, sari);
 insert into users (username, first_name, last_name) values (kurniawan_digital_skola, kurniawan, agung);
 insert into users (username, first_name, last_name) values (adam_digital_skola, adam, makmur);
+insert into users (username, first_name, last_name) values (dito_digital_skola, dito, setyadhi);
+insert into users (username, first_name, last_name) values (kendrick_digital_skola, kendrick, lamar);
