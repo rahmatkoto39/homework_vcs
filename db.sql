52c87dda91887c1897b68175ca7796642b386220
dito-setyadhi <setyadhidito29@gmail.com>
2021-08-23 13:28:44 +0700

    test pertama kali

--- db.sql
+++ db.sql
@@ -2,3 +2,6 @@ insert into users (username, first_name, last_name) values (rahmat_digital_skola
 insert into users (username, first_name, last_name) values (lela_digital_skola, lela, sari);
 insert into users (username, first_name, last_name) values (kurniawan_digital_skola, kurniawan, agung);
 insert into users (username, first_name, last_name) values (adam_digital_skola, adam, makmur);
+insert into users (username, first_name, last_name) values (Dito_Digital_Skola, Dito, Setyadhi);
+insert into users (username, first_name, last_name) values (Rido_Digital_Skola, Rido, Aryadhi);
+insert into users (username, first_name, last_name) values (Dira_Digital_Skola, Dira, Syafitri);
